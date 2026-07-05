1f95c2800ff4c35b0c11205c4735969aaf1d4d9c
Jonatas Sampaio <aurijona192@gmail.com>
2026-07-05 04:53:41 -0300

    Implementando ListTransactionByCategoryUseCase em TransactionController

--- src/main/java/br/com/jonathan/budgeting/infrastructure/http/TransactionController.java
+++ src/main/java/br/com/jonathan/budgeting/infrastructure/http/TransactionController.java
@@ -1,15 +1,21 @@
 package br.com.jonathan.budgeting.infrastructure.http;
 
+import br.com.jonathan.budgeting.application.ListTransactionByCategoryUseCase;
 import br.com.jonathan.budgeting.application.PersistTransactionUseCase;
 import br.com.jonathan.budgeting.application.output.TransactionOutput;
+import br.com.jonathan.budgeting.domain.Category;
 import br.com.jonathan.budgeting.infrastructure.http.request.TransactionRequest;
 import br.com.jonathan.budgeting.infrastructure.http.response.TransactionResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
@@ -17,8 +23,11 @@ public class TransactionController {
 
     private final PersistTransactionUseCase persistTransactionUseCase;
 
-    public TransactionController(PersistTransactionUseCase persistTransactionUseCase) {
+    private final ListTransactionByCategoryUseCase listTransactionByCategoryUseCase;
+
+    public TransactionController(PersistTransactionUseCase persistTransactionUseCase, ListTransactionByCategoryUseCase listTransactionByCategoryUseCase) {
         this.persistTransactionUseCase = persistTransactionUseCase;
+        this.listTransactionByCategoryUseCase = listTransactionByCategoryUseCase;
     }
 
     @PostMapping
@@ -30,5 +39,18 @@ public class TransactionController {
         TransactionResponse response = TransactionResponse.from(output);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{category}")
+    public ResponseEntity<List<TransactionResponse>> findAllByCategory(
+            @PathVariable Category category
+    ) {
+        List<TransactionResponse> response = listTransactionByCategoryUseCase
+                .execute(category)
+                .stream()
+                .map(TransactionResponse::from)
+                .toList();
+
+        return ResponseEntity.ok(response);
     }
 }
